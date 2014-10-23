18bbd27b7008301a9c05952ddc3b3a1cfc20d178
winhong <huanghuangmaojian@huangmaojiandeiMac.local>
2014-10-23 11:01:14 +0800

    修改其他页label名称

--- WinCenter/WinCenter/controllers/pool/PoolDashboardCell.h
+++ WinCenter/WinCenter/controllers/pool/PoolDashboardCell.h
@@ -11,21 +11,12 @@
 
 @interface PoolDashboardCell : UICollectionViewCell
 @property (weak, nonatomic) IBOutlet UILabel *title;
-@property (weak, nonatomic) IBOutlet UIProgressView *progress;
-@property (weak, nonatomic) IBOutlet UILabel *type;
-@property (weak, nonatomic) IBOutlet UILabel *status;
-@property (weak, nonatomic) IBOutlet UILabel *share;
-@property (weak, nonatomic) IBOutlet UIImageView *share_image;
 @property (weak, nonatomic) IBOutlet UILabel *hostCount;
 @property (weak, nonatomic) IBOutlet UILabel *activeVmCount;
 @property (weak, nonatomic) IBOutlet UILabel *cpuUnitCount;
 @property (weak, nonatomic) IBOutlet UILabel *memerySize;
 @property (weak, nonatomic) IBOutlet UILabel *storageSize;
 
-@property (weak, nonatomic) IBOutlet UILabel *osType;
-@property (weak, nonatomic) IBOutlet UIImageView *osType_image;
-@property (weak, nonatomic) IBOutlet UIImageView *linkState;
-@property (weak, nonatomic) IBOutlet UIImageView *type_image;
 @property (weak, nonatomic) IBOutlet F3BarGauge *progress_1;
 @property (weak, nonatomic) IBOutlet F3BarGauge *progress_2;
 @property (weak, nonatomic) IBOutlet F3BarGauge *progress_3;
