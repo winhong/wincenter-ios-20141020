53f44fdb2847c3139a3cdb92f6546f7f8764d9e2
179309463 <179309463@qq.com>
2014-10-24 17:50:22 +0800

    splitview

---
+++ WinCenter/WinCenter/controllers/dashboard/DashboardSplitVC.h
@@ -0,0 +1,13 @@
+//
+//  DashboardSplitVC.h
+//  WinCenter
+//
+//  Created by apple on 14/10/24.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardSplitVC : UISplitViewController
+
+@end
