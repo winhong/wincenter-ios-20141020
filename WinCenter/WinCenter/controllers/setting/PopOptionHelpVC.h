45a9da0d1d40b7d39585db736a4f004fe4ffeb37
fengzj <fengzhuojia@fengzjdeMacBook-Pro.local>
2014-10-27 16:49:10 +0800

    设置中的帮助文档和缺陷反馈修复好了

---
+++ WinCenter/WinCenter/controllers/setting/PopOptionHelpVC.h
@@ -0,0 +1,13 @@
+//
+//  PopOptionHelpVC.h
+//  WinCenter
+//
+//  Created by fengzj on 14/10/27.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import "TOWebViewController.h"
+
+@interface PopOptionHelpVC : TOWebViewController
+
+@end
