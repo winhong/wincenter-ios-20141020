329debe1c6587fef2c329d42f3734f092850ca44
fengzj <fengzhuojia@fengzjdeMacBook-Pro.local>
2014-11-11 19:41:33 +0800

    许可证问题

---
+++ WinCenter/WinCenter/models/LicenseCheckVO.h
@@ -0,0 +1,13 @@
+//
+//  LicenseCheckVO.h
+//  WinCenter
+//
+//  Created by fengzj on 14/11/11.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LicenseCheckVO : NSObject
+@property int errorState;
+@end
--- WinCenter/WinCenter/models/LicenseVO.m
+++ WinCenter/WinCenter/models/LicenseVO.m
@@ -10,6 +10,19 @@
 
 @implementation LicenseVO
 
++ (void) checkLicenseAsync:(FetchObjectCompletionBlock)completeBlock{
+    if([[[NSUserDefaults standardUserDefaults] stringForKey:@"isDemo"] isEqualToString:@"true"]){
+        completeBlock([[LicenseCheckVO alloc] initWithJSONData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"LicenseVO.checkLicenseAsync" ofType:@"json"]]], nil);
+        return;
+    }
+    
+    [[UNIRest get:^(UNISimpleRequest *simpleRequest) {
+        [simpleRequest setUrl:@"/pc/licServlet?action=checkLic"];
+    }] asJsonAsync:^(UNIHTTPJsonResponse *jsonResponse, NSError *error) {
+        completeBlock([[LicenseCheckVO alloc] initWithJSONData:jsonResponse.rawBody], error);
+    }];
+}
+
 + (void) getLicenseVOAsync:(FetchObjectCompletionBlock)completeBlock{
     if([[[NSUserDefaults standardUserDefaults] stringForKey:@"isDemo"] isEqualToString:@"true"]){
         completeBlock([[LicenseVO alloc] initWithJSONData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"LicenseVO.getLicenseVOAsync" ofType:@"json"]]], nil);
