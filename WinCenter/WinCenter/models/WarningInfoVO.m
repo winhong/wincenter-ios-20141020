05b338b6a5cf8ad996e45ad56ac5862286412a60
fengzj <fengzhuojia@fengzjdeMacBook-Pro.local>
2014-11-11 17:01:04 +0800

    详情中的告警信息

--- WinCenter/WinCenter/models/WarningInfoVO.m
+++ WinCenter/WinCenter/models/WarningInfoVO.m
@@ -11,14 +11,25 @@
 @implementation WarningInfoVO
 
 
-+ (void) getWarningInfoListAsync:(FetchObjectCompletionBlock)completeBlock referTo:(NSMutableArray*)referList{
++ (void) getWarningInfoListViaObject:(RemoteObject*)remoteObject Async:(FetchObjectCompletionBlock)completeBlock referTo:(NSMutableArray*)referList{
+    
     if([[[NSUserDefaults standardUserDefaults] stringForKey:@"isDemo"] isEqualToString:@"true"]){
         completeBlock([[WarningInfoListResult alloc] initWithJSONData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"WarningInfoVO.getWarningInfoListAsync" ofType:@"json"]]], nil);
         return;
     }
     
     [[UNIRest get:^(UNISimpleRequest *simpleRequest) {
-        [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, referList.count, referList.count+per_page]];
+        if(remoteObject==nil){
+            [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, referList.count, referList.count+per_page]];
+        }else if([remoteObject isKindOfClass:[PoolVO class]]){
+            [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=objectId%%3D%d%%26objectType%%3DRESOURCE_POOL%%26firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, ((PoolVO*)remoteObject).resourcePoolId, referList.count, referList.count+per_page]];
+        }else if([remoteObject isKindOfClass:[HostVO class]]){
+            [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=objectId%%3D%d%%26objectType%%3DHOST%%26firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, ((HostVO*)remoteObject).hostId, referList.count, referList.count+per_page]];
+        }else if([remoteObject isKindOfClass:[VmVO class]]){
+            [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=objectId%%3D%d%%26objectType%%3DVM%%26firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, ((VmVO*)remoteObject).vmId, referList.count, referList.count+per_page]];
+        }else if([remoteObject isKindOfClass:[StorageVO class]]){
+            [simpleRequest setUrl:[NSString stringWithFormat:@"/restServlet?connectorId=%d&apiKey=pc.winserver.warning.getVmWarningList&params=objectId%%3D%d%%26objectType%%3DSTORAGE%%26firstResult%%3D%ld%%26maxResult%%3D%ld", [RemoteObject getCurrentDatacenterVO].id, ((StorageVO*)remoteObject).storagePoolId, referList.count, referList.count+per_page]];
+        }
     }] asJsonAsync:^(UNIHTTPJsonResponse *jsonResponse, NSError *error) {
         completeBlock([[WarningInfoListResult alloc] initWithJSONData:jsonResponse.rawBody], error);
     }];
